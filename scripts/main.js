95401c3be3cd5a0cac9961767ec28b6c22caa506
unknown <xzimur@gmail.com>
2020-03-10 22:19:44 +0200

    0.0a

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,7 @@ require("blocks/production/rock-crusher")
 require("blocks/production/sulfur-extractor")
 require("blocks/production/heavy-oil-processor")
 require("blocks/production/light-oil-processor")
+
+require("blocks/distribution/omni-bridge-conveyor")
+
+require("blocks/liquid/omni-bridge-conduit")
