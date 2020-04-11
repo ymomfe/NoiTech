ba02f280d7e6ed01209c9afeb69711d4eee2101d
unknown <xzimur@gmail.com>
2020-04-11 12:51:43 +0300

    2.8a

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
+print("Loading [sky]NoiTech[].")
+
 //blocks
 require("blocks/production")
+print("[lightgray]blocks/production.js - is successfully loaded.")
+
 require("blocks/drills")
+print("[lightgray]blocks/drills.js - is successfully loaded.")
+
 require("blocks/distribution")
+print("[lightgray]blocks/distribution.js - is successfully loaded.")
+
 require("blocks/liquid")
+print("[lightgray]blocks/liquid.js - is successfully loaded.")
+
 require("blocks/power")
+print("[lightgray]blocks/power.js - is successfully loaded.")
+
+//final print
+print("[sky]NoiTech[] is successfully loaded.")
