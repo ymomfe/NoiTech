4df43a55c6f4e89b0fdc5450dec88e3ef039eab6
unknown <xzimur@gmail.com>
2020-04-30 21:43:52 +0300

    3.b

--- scripts/blocks/xzimur-noise-production.js
+++ scripts/blocks/xzimur-noise-production.js
@@ -15,8 +15,25 @@ const mineralExtractor = extendContent(GenericCrafter, "mineral-extractor", {
 	},
 });
 
-/*Sludge Recycler*/
-const sludgeRecycler = extendContent(GenericCrafter, "sludge-recycler", {
+/*Heavy Oil Processor*/
+const heavyOilProcessor = extendContent(GenericCrafter, "heavy-oil-processor", {
+	draw(tile){
+		Draw.rect(Core.atlas.find(this.name + "-bottom"), tile.drawx(), tile.drawy());
+		Draw.color(this.outputLiquid.liquid.color);
+		Draw.alpha(tile.entity.liquids.get(this.outputLiquid.liquid) / this.liquidCapacity);
+		Draw.rect(Core.atlas.find(this.name + "-liquid"), tile.drawx(), tile.drawy());
+		Draw.color();
+		Draw.rect(Core.atlas.find(this.name + "-top"), tile.drawx(), tile.drawy());
+	},
+	generateIcons: function(){
+		return [
+			Core.atlas.find(this.name)
+		];
+	},	
+});
+
+/*Light Oil Processor*/
+const lightOilProcessor = extendContent(GenericCrafter, "light-oil-processor", {
 	draw(tile){
 		Draw.rect(Core.atlas.find(this.name + "-bottom"), tile.drawx(), tile.drawy());
 		Draw.color(this.outputLiquid.liquid.color);
